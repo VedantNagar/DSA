f8524759a9ffa4bebaa84c576db7116e8a3cceaa
Vedant <vedantnagar21@gmail.com>
2024-02-05 22:30:38 +0530

    stack

---
+++ Stack/Extras/Next smaller element.cpp
@@ -0,0 +1,22 @@
+/* You are given an array 'ARR' of integers of length N. Your task is to find the next smaller element for each of the array elements.
+
+Next Smaller Element for an array element is the first element to the right of that element which has a value strictly smaller than that element. */
+
+#include <bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int>s;
+    vector<int>ans(n);
+    s.push(-1);
+
+    for(int i=n-1;i>=0;i--){
+        int curr=arr[i];
+
+        while(s.top()>=curr){
+            s.pop();
+        }
+        ans[i]=s.top();
+        s.push(curr);
+    }
+    return ans;
+}
